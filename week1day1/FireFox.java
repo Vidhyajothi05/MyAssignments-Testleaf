3d6b073db596f0834f4aa5841458d1b5f03b27ed
Vidhyajothi05 <vidhya.cluny@gmail.com>
2025-12-07 12:43:22 +0530

    Add files via upload

---
+++ week1day1/FireFox.java
@@ -0,0 +1,22 @@
+package week1day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float browserVersion=100.2f;
+		String browserName="FireFox";
+		boolean isVisible=true;
+	    int releaseYear=1998;
+	    char browserLogo='F';
+	    System.out.println(browserVersion+"\n"+browserName+"\n"+isVisible+"\n"+releaseYear+"\n"+browserLogo+"\n");
+	
+	    //System.out.println(browserVersion);
+	
+	
+	
+	
+	}
+
+}
